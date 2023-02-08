9f2c7c0c08b910ca457efb8e8bcebf73f6ddb0d5
Zehra <busraebrar2014@hotmail.com>
2023-02-08 12:51:45 -0500

    added user

---
+++ m8-new-version-updates/src/main/java/com/cydeo/java14/User.java
@@ -0,0 +1,11 @@
+package com.cydeo.java14;
+
+public record User(String name,String address) {
+  // // private - final // //
+
+    //ALL ARG CONST
+    //GETTER
+    //TO STRING
+    //EQUALS
+    //HASH CODE
+}
